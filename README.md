44a8a6fa14d726c77b606b95faa9b676fd992568
ksauzz <ksauzzmsg@gmail.com>
2014-11-18 20:51:55 +0900

    specified grafana version.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ So you can aggregate metrics from multiple servers using collectd or something v
 ```
 sudo docker pull ksauzz/elasticsearch
 sudo docker pull ksauzz/influxdb:grafana
-sudo docker pull ksauzz/grafana
+sudo docker pull ksauzz/grafana:1.6.0
 sudo ./run-grafana.sh
 ./init-db.sh
 ```
--- run-grafana.sh
+++ run-grafana.sh
@@ -12,10 +12,10 @@ fi
 set -x
 
 docker run -d -p 8086:8086 -p 8083:8083 -p 2003:2003 \
-  -v $BASE_DIR/influxdb:/opt/influxdb/shared/data -i ksauzz/influxdb > .container_ids
+  -v $BASE_DIR/influxdb:/opt/influxdb/shared/data -i ksauzz/influxdb:grafana > .container_ids
 docker run -d -p 9200:9200 \
   -v $BASE_DIR/elasticsearch/data:/var/lib/elasticsearch \
   -v $BASE_DIR/elasticsearch/log:/var/log/elasticsearch \
   -i ksauzz/elasticsearch >> .container_ids
 docker run -d -p 8080:8080 \
-  -v $BASE_DIR/grafana:/var/log/nginx ksauzz/grafana >> .container_ids
+  -v $BASE_DIR/grafana:/var/log/nginx ksauzz/grafana:1.6.0 >> .container_ids
